154fb752ac0d6489fcace85aa2d36782265e0715
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-26 12:23:23 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Daki A.B
+ * Created on: Oct 2023
+ * This program turns neopixels red if sonar measures distance less than 10cm, and turns neopixels green if sonar measures more than 10cm
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceFromObject: number = 0
+
+// on startup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// measuring distance with sonar
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  distanceFromObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+)
+  basic.showNumber(distanceFromObject)
+  basic.showString('cm')
+  basic.showIcon(IconNames.Happy)
+})
